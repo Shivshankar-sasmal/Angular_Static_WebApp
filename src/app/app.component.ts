87595a29ac84f024f90427849145843a257bfc34
Shivshankar_Sasmal <shivshankar72242@gmail.com>
2024-06-01 14:55:02 +0530

    Angular Start 01 Jun 2024

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { HomeComponent } from './sections/home/home.component';
+import { AboutComponent } from './sections/about/about.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    RouterModule,
+    HomeComponent,
+    AboutComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'WebApp';
+  title = 'PORTFOLIO';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './sections/home/home.component';
+import { AboutComponent } from './sections/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path : '', component : HomeComponent },
+    { path : 'about', component : AboutComponent },
+];
